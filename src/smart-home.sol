303056bcb52172398db63846ede9d59c5c563082
smartcoin1207 <jackle.some99@gmail.com>
2023-07-24 12:03:05 +0200

    Update smart-home.sol

--- src/smart-home.sol
+++ src/smart-home.sol
@@ -1,35 +1,88 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity >=0.6.0 <0.9.0;
+
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
+
+// ============================================================================
+// Contracts
+// ============================================================================
+
+contract TemperatureControlContract {
+    function setTemperature(uint temperature) public { /* ... */ }
+}
+
+contract LightControlContract {
+    function setLightIntensity(uint lightIntensity) public { /* ... */ }
+}
+
+contract SecurityAlertContract {
+    function setAlertStatus(bool status) public { /* ... */ }
+}
+
 contract SmartHomeContract {
-    
+
+    // Parameters
+    // ========================================================================
+
     address private owner;
     
-    // Setting a threshold temperature, light intensity and security alert in percentage
     uint public thresholdTemperature = 25;
     uint public thresholdLightIntensity = 500;
     uint public thresholdSecurityAlert = 90;
 
-    // Address of the Chainlink Oracle for temperature, light intensity and security alert respectively
-    AggregatorV3Interface internal temperatureFeed;
-    AggregatorV3Interface internal lightIntensityFeed;
-    AggregatorV3Interface internal securityAlertFeed;
 
-    // Events that will be emitted when threshold is crossed
-    event TemperatureThresholdCrossed(uint temperature);
-    event LightIntensityThresholdCrossed(uint lightIntensity);
-    event SecurityAlertThresholdCrossed(uint securityAlert);
+    // Constructor
+    // ========================================================================
 
-    constructor(address _temperatureFeed, address _lightIntensityFeed, address _securityAlertFeed) public {
+    constructor(
+        address _temperatureFeed, 
+        address _lightIntensityFeed, 
+        address _securityAlertFeed,
+        TemperatureControlContract _temperatureControl,
+        LightControlContract _lightControl,
+        SecurityAlertContract _securityAlertControl
+    ) public {
         owner = msg.sender;
         temperatureFeed = AggregatorV3Interface(_temperatureFeed);
         lightIntensityFeed = AggregatorV3Interface(_lightIntensityFeed);
         securityAlertFeed = AggregatorV3Interface(_securityAlertFeed);
+        temperatureControl = _temperatureControl;
+        lightControl = _lightControl;
+        securityAlertControl = _securityAlertControl;
     }
 
-    // Check the temperature, light intensity and security alert respectively
+
+    // Aggregators
+    // ========================================================================
+
+    AggregatorV3Interface internal temperatureFeed;
+    AggregatorV3Interface internal lightIntensityFeed;
+    AggregatorV3Interface internal securityAlertFeed;
+
+
+    // Contracts
+    // ========================================================================
+
+    // Adding the contracts for temperature, light and security control
+    TemperatureControlContract public temperatureControl;
+    LightControlContract public lightControl;
+    SecurityAlertContract public securityAlertControl;
+
+
+    // Events
+    // ========================================================================
+
+    event TemperatureThresholdCrossed(uint temperature);
+    event LightIntensityThresholdCrossed(uint lightIntensity);
+    event SecurityAlertThresholdCrossed(uint securityAlert);
+
+
+    // Methods
+    // ========================================================================
+
     function checkTemperature() public {
         (
             uint80 roundID, 
@@ -39,8 +92,10 @@ contract SmartHomeContract {
             uint80 answeredInRound
         ) = temperatureFeed.latestRoundData();
         
-        if(uint(answer) > thresholdTemperature) {
-            emit TemperatureThresholdCrossed(uint(answer));
+        uint temperature = uint(answer);
+        if(temperature > thresholdTemperature) {
+            emit TemperatureThresholdCrossed(temperature);
+            temperatureControl.setTemperature(thresholdTemperature); // reduce temperature
         }
     }
 
@@ -53,8 +108,10 @@ contract SmartHomeContract {
             uint80 answeredInRound
         ) = lightIntensityFeed.latestRoundData();
         
-        if(uint(answer) > thresholdLightIntensity) {
-            emit LightIntensityThresholdCrossed(uint(answer));
+        uint lightIntensity = uint(answer);
+        if(lightIntensity > thresholdLightIntensity) {
+            emit LightIntensityThresholdCrossed(lightIntensity);
+            lightControl.setLightIntensity(thresholdLightIntensity); // reduce light intensity
         }
     }
 
@@ -67,8 +124,11 @@ contract SmartHomeContract {
             uint80 answeredInRound
         ) = securityAlertFeed.latestRoundData();
         
-        if(uint(answer) > thresholdSecurityAlert) {
-            emit SecurityAlertThresholdCrossed(uint(answer));
+        uint securityAlert = uint(answer);
+        if(securityAlert > thresholdSecurityAlert) {
+            emit SecurityAlertThresholdCrossed(securityAlert);
+            securityAlertControl.setAlertStatus(true); // trigger alert
         }
     }
+
 }
