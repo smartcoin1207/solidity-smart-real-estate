967992be2ff40b2ecea3418eb2273024a41c5303
smartcoin1207 <jackle.some99@gmail.com>
2023-07-24 12:25:28 +0200

    Update smart-home.sol

--- src/smart-home.sol
+++ src/smart-home.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0
 
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity ^0.8.19;
 
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
