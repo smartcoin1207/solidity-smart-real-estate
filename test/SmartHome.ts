418698a527daab1d7d6a9b83a57a1fb75dc0d894
smartcoin1207 <jackle.some99@gmail.com>
2023-08-01 09:38:49 +0200

    Update SmartHome.ts

--- test/SmartHome.ts
+++ test/SmartHome.ts
@@ -1,127 +1,137 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { expect } from "chai";
+// Copyright 2023 Stichting Block Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+// Import the required modules and types for testing
 import { ethers } from "hardhat";
+import chai from "chai";
+import { solidity } from "ethereum-waffle";
+import { TemperatureControlContract } from "../typechain/TemperatureControlContract";
+import { LightControlContract } from "../typechain/LightControlContract";
+import { SecurityAlertContract } from "../typechain/SecurityAlertContract";
+import { SmartHomeContract } from "../typechain/SmartHomeContract";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+// Use solidity testing capabilities from ethereum-waffle
+chai.use(solidity);
+const { expect } = chai;
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+// Test suite for the SmartHomeContract
+describe("SmartHomeContract", function () {
+  let temperatureControl: TemperatureControlContract;
+  let lightControl: LightControlContract;
+  let securityAlertControl: SecurityAlertContract;
+  let smartHome: SmartHomeContract;
+  let owner: SignerWithAddress;
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+  // Runs before each test in the suite, setting up contract instances for testing
+  beforeEach(async function () {
+    // Get contract factories for deploying instances
+    const TemperatureControlFactory = await ethers.getContractFactory("TemperatureControlContract");
+    const LightControlFactory = await ethers.getContractFactory("LightControlContract");
+    const SecurityAlertFactory = await ethers.getContractFactory("SecurityAlertContract");
+    const SmartHomeFactory = await ethers.getContractFactory("SmartHomeContract");
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    [owner] = await ethers.getSigners();
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
+    // Deploy contract instances for testing
+    temperatureControl = (await TemperatureControlFactory.deploy()) as TemperatureControlContract;
+    lightControl = (await LightControlFactory.deploy()) as LightControlContract;
+    securityAlertControl = (await SecurityAlertFactory.deploy()) as SecurityAlertContract;
+    smartHome = (await SmartHomeFactory.deploy(
+        "0xSomeAddress", // temperature feed
+        "0xSomeAddress", // light intensity feed
+        "0xSomeAddress", // security alert feed
+        temperatureControl.address,
+        lightControl.address,
+        securityAlertControl.address
+    )) as SmartHomeContract;
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
+    // Make sure contract was deployed successfully
+    await smartHome.deployed();
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  // Test case: owner can update threshold values
+  it("Should correctly set thresholds", async function () {
+    await smartHome.setThresholdTemperature(30);
+    await smartHome.setThresholdLightIntensity(600);
+    await smartHome.setThresholdSecurityAlert(80);
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
+    expect(await smartHome.thresholdTemperature()).to.equal(30);
+    expect(await smartHome.thresholdLightIntensity()).to.equal(600);
+    expect(await smartHome.thresholdSecurityAlert()).to.equal(80);
+  });
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+  // Test case: non-owner cannot update threshold values
+  it("Should fail when non-owner attempts to set thresholds", async function () {
+    const [_, nonOwner] = await ethers.getSigners();
 
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
+    await expect(
+      smartHome.connect(nonOwner).setThresholdTemperature(30)
+    ).to.be.revertedWith("Caller is not owner");
 
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
+    await expect(
+      smartHome.connect(nonOwner).setThresholdLightIntensity(600)
+    ).to.be.revertedWith("Caller is not owner");
 
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    await expect(
+      smartHome.connect(nonOwner).setThresholdSecurityAlert(80)
+    ).to.be.revertedWith("Caller is not owner");
+  });
 
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
+  // Test case: contract emits event when temperature threshold is exceeded
+  it("Should emit TemperatureThresholdCrossed event when threshold is exceeded", async function () {
+    // We need to have a way of controlling the data returned by temperatureFeed.latestRoundData()  
+    await expect(smartHome.checkTemperature())
+      .to.emit(smartHome, "TemperatureThresholdCrossed")
+      .withArgs(/* expected temperature value */);
+  });
 
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
+  // Test case: contract emits event when light intensity threshold is exceeded
+  it("Should emit LightIntensityThresholdCrossed event when threshold is exceeded", async function () {
+    // We need to have control over lightIntensityFeed.latestRoundData()    
+    await expect(smartHome.checkLightIntensity())
+      .to.emit(smartHome, "LightIntensityThresholdCrossed")
+      .withArgs(/* expected light intensity value */);
+  });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+  // Test case: contract emits event when security alert threshold is exceeded
+  it("Should emit SecurityAlertThresholdCrossed event when threshold is exceeded", async function () {
+    // We need to have control over securityAlertFeed.latestRoundData()    
+    await expect(smartHome.checkSecurityAlert())
+      .to.emit(smartHome, "SecurityAlertThresholdCrossed")
+      .withArgs(/* expected security alert value */);
+  });
 
-        await time.increaseTo(unlockTime);
+  // Test case: contract correctly updates temperature in TemperatureControlContract
+  it("Should set correct temperature in TemperatureControlContract", async function () {
+    // Assume that temperatureFeed.latestRoundData() returns a value higher than the threshold    
+    await smartHome.checkTemperature();
+    expect(await temperatureControl.temperature()).to.equal(await smartHome.thresholdTemperature());
+  });
 
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
+  // Test case: contract correctly updates light intensity in LightControlContract
+  it("Should set correct light intensity in LightControlContract", async function () {
+    // Assume that lightIntensityFeed.latestRoundData() returns a value higher than the threshold    
+    await smartHome.checkLightIntensity();
+    expect(await lightControl.lightIntensity()).to.equal(await smartHome.thresholdLightIntensity());
+  });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
+  // Test case: contract correctly updates alert status in SecurityAlertContract
+  it("Should set alert status in SecurityAlertContract", async function () {
+    // Assume that securityAlertFeed.latestRoundData() returns a value higher than the threshold    
+    await smartHome.checkSecurityAlert();
+    expect(await securityAlertControl.status()).to.be.true;
   });
 });
