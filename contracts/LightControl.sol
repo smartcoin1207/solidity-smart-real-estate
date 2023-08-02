4564be5c65e388bcdfb083c4833ce21b392f2dba
smartcoin1207 <jackle.some99@gmail.com>
2023-08-02 17:05:17 +0200

    Update LightControl.sol

--- contracts/LightControl.sol
+++ contracts/LightControl.sol
@@ -16,10 +16,17 @@
 // limitations under the License.
 
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.7; 
 
 
-// import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+// ============================================================================
+// Import
+// ============================================================================
+
+// Remote
+import '@chainlink/contracts/src/v0.8/ChainlinkClient.sol'; 
+import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol'; 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 
 // ============================================================================
